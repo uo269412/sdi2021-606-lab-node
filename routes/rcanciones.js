c5d5b875000f621d1920e9e03a19bb4942c1600f
uo269412 <uo269412@uniovi.es>
2021-03-22 16:04:49 +0100

    SDI-2021-606-7.5-Vistas y motores de plantillas.

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -1,10 +1,23 @@
-module.exports = function(app) {
-    app.get("/canciones", function(req, res) { 
-        let respuesta = "";
-        if (req.query.nombre != null)
-            respuesta += 'Nombre: ' + req.query.nombre + '<br>';
-        if (typeof  (req.query.autor) != "undefined")
-            respuesta += 'Autor: ' + req.query.autor;
+module.exports = function(app, swig) {
+    app.get("/canciones", function(req, res) {
+
+        let canciones = [ {
+                "nombre": "Blank space",
+                "precio": "1.2"
+            }, {
+                "nombre": "See you again",
+                "precio": "1.3"
+            }, {
+                "nombre": "Uptown Funk",
+                "precio": "1.1"
+            } ];
+
+        let respuesta = swig.renderFile('views/btienda.html', {
+            vendedor: 'Tienda de canciones',
+            canciones: canciones
+        });
+
+
         res.send(respuesta);
     });
 
@@ -12,6 +25,13 @@ module.exports = function(app) {
         let respuesta = parseInt(req.query.num1) + parseInt(req.query.num2);
         res.send(String(respuesta));
     });
+
+    app.get('/canciones/agregar', function (req, res) {
+        let respuesta = swig.renderFile('views/bagregar.html', {
+
+        });
+        res.send(respuesta);
+    })
 
     app.get('/canciones/:id', function(req, res) {
         let respuesta = 'id: ' + req.params.id;
@@ -27,6 +47,7 @@ module.exports = function(app) {
     app.post('/cancion', function(req, res) {
         res.send("Canción agregada: " + req.body.nombre + "<br>" + " genero: " + req.body.genero + "<br>" + " precio: " + req.body.precio);
     });
+
 
     app.get('/promo*', function (req, res) {
         res.send('Respuesta patrón promo* ');
