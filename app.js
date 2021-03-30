df372c274c013ea7acb81df443acd448027c9bf6
uo269412 <uo269412@uniovi.es>
2021-03-30 09:07:02 +0200

    SDI-2021-606-8.2-Arquitectura para el acceso a datos.

--- app.js
+++ app.js
@@ -9,14 +9,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+let gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
 
 //Variables
 app.set('port', 8081);
 app.set('db','mongodb://admin:sdi@tiendamusica-shard-00-00.ywrsn.mongodb.net:27017,tiendamusica-shard-00-01.ywrsn.mongodb.net:27017,tiendamusica-shard-00-02.ywrsn.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-8gbctl-shard-0&authSource=admin&retryWrites=true&w=majority');
 
 //Rutas/controladores por lógica
-require("./routes/rusuarios.js")(app, swig); // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app, swig, mongo); // (app, param1, param2, etc.)
+require("./routes/rusuarios.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
 require("./routes/rautores.js")(app, swig); // (app, param1, param2, etc.)
 
 //Lanzar el servidor
