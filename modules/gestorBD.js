60f29acdea6bb69f2a8228358891910d9c23917e
uo269412 <uo269412@uniovi.es>
2021-04-05 17:22:22 +0200

    SDI-2021-606-9.5-Sistema de paginación.

--- modules/gestorBD.js
+++ modules/gestorBD.js
@@ -3,6 +3,28 @@ module.exports = { mongo : null, app : null,
         this.mongo = mongo;
         this.app = app;
     },
+
+    obtenerCancionesPg : function(criterio,pg,funcionCallback){
+        this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                let collection = db.collection('canciones');
+                collection.count(function(err, count){
+                    collection.find(criterio).skip( (pg-1)*4 ).limit( 4 )
+                        .toArray(function(err, canciones) {
+                            if (err) {
+                                funcionCallback(null);
+                            } else {
+                                funcionCallback(canciones, count);
+                            }
+                            db.close();
+                        });
+                });
+            }
+        });
+    },
+
     insertarUsuario : function(usuario, funcionCallback) { this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
         if (err) {
             funcionCallback(null);
